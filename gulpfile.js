a8a440e7c48f1f0b986c2075678a6c77f76d29fb
@Scriptus <zonder2009@gmail.com>
2018-10-21 17:41:37 +0300

    font awesome + icon menu in top header

--- gulpfile.js
+++ gulpfile.js
@@ -30,7 +30,7 @@ gulp.task('styles', function() {
 	.pipe(sass({ outputStyle: 'expanded' }).on("error", notify.onError()))
 	.pipe(rename({ suffix: '.min', prefix : '' }))
 	.pipe(autoprefixer(['last 15 versions']))
-	.pipe(cleancss( {level: { 1: { specialComments: 0 } } })) // Opt., comment out when debugging
+	//.pipe(cleancss( {level: { 1: { specialComments: 0 } } })) // Opt., comment out when debugging
 	.pipe(gulp.dest('app/css'))
 	.pipe(browserSync.stream())
 });
